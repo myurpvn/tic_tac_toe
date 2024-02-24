0e6ad0c09fdac76ebeeef5f7e20b602701ac7faa
myuR <myurpvn@gmail.com>
2024-02-24 20:14:56 +0530

    add better class properties

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-# Example file showing a circle moving on screen
 import pygame
 
-# pygame setup
 pygame.init()
-screen = pygame.display.set_mode((400, 400))
 pygame.display.set_caption("The Tic-Tac-Toe Game")
-font = pygame.font.Font("freesansbold.ttf", 32)
-half_width = screen.get_width() / 2
-half_height = screen.get_height() / 2
-clock = pygame.time.Clock()
-running = True
-game_over = False
-dt = 0
 
-player_1 = True
+screen_size = (400, 400)
+half_width = screen_size[0] / 2
+half_height = screen_size[1] / 2
+
+screen_center = pygame.Vector2(half_width, half_height)
+screen = pygame.display.set_mode(screen_size)
+clock = pygame.time.Clock()
+
+font = pygame.font.Font("freesansbold.ttf", 32)
+game_music = pygame.mixer.Sound("sound/hot_pursuit.mp3")
+victory_music = pygame.mixer.Sound("sound/new_hero_in_town.mp3")
+draw_music = pygame.mixer.Sound("sound/dark_star.mp3")
+
+running = True
+dt = 0
 
 CENTERS = {
     "c_1": pygame.Vector2(half_width - 100, half_height + 100),
@@ -27,15 +31,10 @@ CENTERS = {
     "c_9": pygame.Vector2(half_width + 100, half_height - 100),
 }
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-game_music = pygame.mixer.Sound("sound/hot_pursuit.mp3")
-victory_music = pygame.mixer.Sound("sound/new_hero_in_town.mp3")
-draw_music = pygame.mixer.Sound("sound/dark_star.mp3")
-
 
 class Game:
     def __init__(self) -> None:
+        self.game_over = False
         self.draw = False
         self.winner = ""
 
@@ -55,12 +54,13 @@ class Player:
             "c_9": False,
         }
         self.won = False
+        self.turn = False
 
     def get_centers(self) -> None:
         click = pygame.mouse.get_pressed()
         if click[0]:
             pos = pygame.mouse.get_pos()
-            min_dist = 99999
+            min_dist = 9999
             center = ""
 
             for i in CENTERS:
@@ -77,6 +77,7 @@ class Circle(Player):
     def __init__(self) -> None:
         super().__init__()
         self.name = "circle"
+        self.turn = True
 
     def draw(self, surface) -> None:
         for center in self.centers:
@@ -94,6 +95,7 @@ class Cross(Player):
     def draw(self, surface) -> None:
         for center in self.centers:
             if self.centers[center]:
+
                 line_1_start = pygame.Vector2(
                     CENTERS[center].x + 40, CENTERS[center].y + 40
                 )
@@ -106,20 +108,20 @@ class Cross(Player):
                 line_2_end = pygame.Vector2(
                     CENTERS[center].x - 40, CENTERS[center].y + 40
                 )
-                pygame.draw.line(
-                    surface,
-                    color="blue",
-                    start_pos=line_1_start,
-                    end_pos=line_1_end,
-                    width=5,
-                )
-                pygame.draw.line(
-                    surface,
-                    color="blue",
-                    start_pos=line_2_start,
-                    end_pos=line_2_end,
-                    width=5,
-                )
+
+                lines = {
+                    "line_1": (line_1_start, line_1_end),
+                    "line_2": (line_2_start, line_2_end),
+                }
+
+                for line in lines:
+                    pygame.draw.line(
+                        surface,
+                        color="blue",
+                        start_pos=lines[line][0],
+                        end_pos=lines[line][1],
+                        width=5,
+                    )
 
 
 def generate_grid() -> None:
@@ -176,22 +178,24 @@ def check_draw(player_1: Circle, player_2: Cross, game: Game) -> bool:
 
 
 game_music.play(-1)
-
 game = Game()
 circle = Circle()
 cross = Cross()
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if not game_over:
-                if player_1:
+            if not game.game_over:
+                if circle.turn:
+                    circle.turn = False
+                    cross.turn = True
                     circle.get_centers()
-                    player_1 = False
-                else:
+                elif cross.turn:
+                    cross.turn = False
+                    circle.turn = True
                     cross.get_centers()
-                    player_1 = True
 
     screen.fill("grey")
     generate_grid()
@@ -200,24 +204,20 @@ while running:
 
     if circle.won or check_win(circle, game):
         text = font.render("Circle Wins", True, "green", "blue")
-        screen.blit(text, player_pos)
-        game_over = True
+        screen.blit(text, screen_center)
+        game.game_over = True
     elif cross.won or check_win(cross, game):
         text = font.render("Cross Wins", True, "green", "blue")
-        screen.blit(text, player_pos)
-        game_over = True
+        screen.blit(text, screen_center)
+        game.game_over = True
     else:
         if game.draw or check_draw(circle, cross, game):
             text = font.render("Draw", True, "green", "blue")
-            screen.blit(text, player_pos)
-            game_over = True
+            screen.blit(text, screen_center)
+            game.game_over = True
 
-    # flip() the display to put your work on screen
     pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
     dt = clock.tick(60) / 1000
 
 pygame.quit()
